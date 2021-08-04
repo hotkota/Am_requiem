eeea9173824d9ed3a63f3b9bb0c45f5c60e03758
Hot_kota <kisamawk@gmail.com>
2021-08-05 00:10:17 +0700

    hot fix

--- src/event/on_message_reaction_add.cr
+++ src/event/on_message_reaction_add.cr
@@ -5,7 +5,7 @@ module Event
     def message_reaction_add(client, cache, handler, db)
         if !cache.resolve_user(handler.user_id).bot
             Redis.open(database: db) do |redis|
-                if redis.exists(handler.message_id.to_s)
+                if !redis.get(handler.message_id.to_s).nil?
                     if handler.emoji.id.nil?
                         client.create_message(handler.channel_id, "#{cache.resolve_user(handler.user_id).username} нажал на: #{handler.emoji.name}")
                     else
